4b29600d37ee7c9c0206ca0b996eb487bc0daffb
Aayu-shh <aayush28.official@gmail.com>
2023-06-24 11:43:11 +0530

    Added Cart Logic -Add to, Delete from Cart Enabled

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -19,12 +21,12 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     User.findByPk(1)
-    .then(user => {
-        req.user = user;            //Adding user(sequelize object) from DB to req
-        next();
-    });
+        .then(user => {
+            req.user = user;            //Adding user(sequelize object) from DB to req
+            next();
+        });
 });
 
 app.use('/admin', adminRoutes);
@@ -33,27 +35,36 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 //User CREATED this product
-Product.belongsTo(User,{constraints: true, onDelete: 'CASCADE'});             //onDelete => OnDeleteing User, Product data it created also DELETED          
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });             //onDelete => OnDeleteing User, Product data it created also DELETED          
 
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);           //Not needed, Inverse of above assoc
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-//.sync({force: true   })
-.sync()
-.then(res => {
-    return User.findByPk(1); 
-})
-.then(user => {
-    if(!user){
-        User.create({name:'Max',email:'DummyEmail@fmail.com'});
-    }
-    return Promise.resolve(user);
-})
-.then(user =>{
-    //console.log(user);
-    app.listen(3000);
-})
-.catch(err => {
-    console.log(err)
-});
+    //.sync({ force: true })
+    .sync()
+    .then(res => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Max', email: 'DummyEmail@fmail.com' });
+        }
+        return user;
+    })
+    .then(user => {
+        //console.log(user);
+        return user.createCart();
+
+    })
+    .then(cart => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err)
+    });
 
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,7 @@
 //All pages Rendering , CART functionality TO BE added
 const Product = require('../models/product');
 const Cart = require('../models/cart');
+const CartItem = require('../models/cart-item');
 
 exports.getProducts = (req, res, next) => {
   Product.findAll()
@@ -16,11 +17,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findByPk(prodId)                                      //[[product]] --> Because product details(Single) INSIDE array INSIDE another array with products and MetaData
-  .then(product => {
-    res.render('shop/product-detail', { product: product, pageTitle: product.title, path: '/products' });
-  })
-  .catch(err => console.log(err));
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-detail', { product: product, pageTitle: product.title, path: '/products' });
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getIndex = (req, res, next) => {
@@ -32,24 +33,71 @@ exports.getIndex = (req, res, next) => {
         path: '/'
       });
     })
-    .catch(err =>console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  req.user.getCart()
+    .then(cart => {
+      return cart.getProducts()
+        .then(products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err))
+
+
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-  .then(product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } })      //Only retireve our product
+    })
+    .then(products => {         //Holds Only 1 product
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      let newQuantity = 1;
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return fetchedCart.addProduct(product, { through: { quantity: newQuantity } })
+      }
+      return Product.findByPk(prodId)
+        .then(product => {
+          return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
+        })
+        .catch(err => console.log(err));
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err))
 };
+
+exports.deleteCartItem = (req, res, next) => {
+  const prodId = req.body.productId;
+  CartItem.findAll({
+    where: {
+      productId: prodId
+    }
+  })
+    .then(cartItem => {
+      return cartItem[0].destroy()
+    })
+    .then(() => res.redirect('/cart'))
+    .catch(err => console.log(err));
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
